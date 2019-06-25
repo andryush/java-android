f6418649d1a8c1419b3e37c35a79019f31374bfa
andryush <andryush333@gmail.com>
2019-06-25 16:56:10 +0400

    cafeorder: finished order activity

--- Cafeorder/app/src/main/java/com/arakelyan/cafeorder/CreateOrderActivity.java
+++ Cafeorder/app/src/main/java/com/arakelyan/cafeorder/CreateOrderActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -23,7 +24,9 @@ public class CreateOrderActivity extends AppCompatActivity {
 
     private String name;
     private String password;
-    private String hello;
+    private String drink;
+
+    private StringBuilder builderAdditions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,21 +51,87 @@ public class CreateOrderActivity extends AppCompatActivity {
         checkBoxSugar = findViewById(R.id.cb_sugar);
         checkBoxMilk = findViewById(R.id.cb_milk);
         checkBoxLemon = findViewById(R.id.cb_lemon);
-        
+
         spinnerTea = findViewById(R.id.sp_tea);
         spinnerCoffee = findViewById(R.id.sp_coffee);
 
-        hello = getString(R.string.tv_hello_user, name);
+        drink = getString(R.string.rb_tea);
 
+        String additions = String.format(getString(R.string.tv_ingredients), drink);
+        textViewAdditions.setText(additions);
+
+        String hello = String.format(getString(R.string.tv_hello_user), name);
         textViewHello.setText(hello);
+
+        builderAdditions = new StringBuilder();
 
     }
 
     public void changeDrink(View view) {
+        RadioButton button = (RadioButton) view;
+        int id = button.getId();
+
+        if (id == R.id.rb_tea) {
+            drink = getString(R.string.rb_tea);
+            spinnerTea.setVisibility(View.VISIBLE);
+            spinnerCoffee.setVisibility(View.INVISIBLE);
+            checkBoxLemon.setVisibility(View.VISIBLE);
+        }
+        else if (id == R.id.rb_coffee) {
+            drink = getString(R.string.rb_coffee);
+            spinnerCoffee.setVisibility(View.VISIBLE);
+            spinnerTea.setVisibility(View.INVISIBLE);
+            checkBoxLemon.setVisibility(View.INVISIBLE);
+        }
+
+        String additions = String.format(getString(R.string.tv_ingredients), drink);
+
+        textViewAdditions.setText(additions);
 
     }
 
     public void sendOrder(View view) {
 
+        builderAdditions.setLength(0);
+
+        if (checkBoxSugar.isChecked()) {
+            builderAdditions.append(getString(R.string.cb_sugar)).append(" ");
+        }
+
+        if (checkBoxSugar.isChecked()) {
+            builderAdditions.append(getString(R.string.cb_sugar)).append(" ");
+        }
+
+        if (checkBoxLemon.isChecked() && drink.equals(getString(R.string.rb_tea))) {
+            builderAdditions.append(getString(R.string.cb_lemon)).append(" ");
+        }
+
+        String optionsDrink = "";
+
+        if (drink.equals(getString(R.string.rb_tea))) {
+            optionsDrink = spinnerTea.getSelectedItem().toString();
+        }
+        else {
+            optionsDrink = spinnerCoffee.getSelectedItem().toString();
+        }
+
+        String order = String.format("Name: %s\nPassword: %s\n Drink: %s\nDrink type: %s\n", name, password, drink, optionsDrink);
+
+        String additions;
+
+        if (builderAdditions.length() > 0) {
+            additions = getString(R.string.need_additions) + builderAdditions.toString();
+        }
+        else {
+            additions = getString(R.string.no_additions);
+        }
+
+        String fullOrder = order + additions;
+
+        Intent intent = new Intent(this, OrderDetailActivity.class);
+
+        intent.putExtra("fullOrder", fullOrder);
+
+        startActivity(intent);
     }
 }
